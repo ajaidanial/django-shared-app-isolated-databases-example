8e522b48168878b305f6a4d70d9494d9996a0a6f
ajaidanial <ajaidanial@gmail.com>
2022-02-17 19:21:38 +0530

    made dry and moved to the model layer

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,6 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.core.management import call_command
 from django.urls import include, path
 
 from main_app import helpers
@@ -24,9 +23,4 @@ if helpers.is_default_database_synchronized():
     from main_app.models import UserDatabaseTracker
 
     for tracker in UserDatabaseTracker.objects.all():
-        db_name = tracker.database_name
-        settings.DATABASES[db_name] = {
-            "ENGINE": "django.db.backends.sqlite3",
-            "NAME": settings.BASE_DIR / f"{db_name}.sqlite3",
-        }
-        call_command("migrate", database=db_name)
+        tracker.set_up_database_and_configurations()
--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.core.management import call_command
 from django.db import models, DEFAULT_DB_ALIAS
 
 
@@ -8,3 +10,13 @@ class UserDatabaseTracker(models.Model):
 
     user_identifier = models.CharField(max_length=255, unique=True)
     database_name = models.CharField(max_length=255)
+
+    def set_up_database_and_configurations(self):
+        """Adds the db to the settings and creates the db if necessary."""
+
+        db_name = self.database_name
+        settings.DATABASES[db_name] = {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": settings.BASE_DIR / f"{db_name}.sqlite3",
+        }
+        call_command("migrate", database=db_name)
