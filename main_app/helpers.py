19f30de2c4818c3c4a67274144f0c0af3fada691
ajaidanial <ajaidanial@gmail.com>
2022-02-17 20:32:42 +0530

    added exceptions for helpers

--- main_app/helpers.py
+++ main_app/helpers.py
@@ -1,4 +1,4 @@
-from django.db import connections, DEFAULT_DB_ALIAS
+from django.db import DEFAULT_DB_ALIAS, connections
 from django.db.migrations.executor import MigrationExecutor
 
 
@@ -8,8 +8,14 @@ def is_default_database_synchronized():
     to apply the necessary init settings.* configurations.
     """
 
-    connection = connections[DEFAULT_DB_ALIAS]
-    connection.prepare_database()
-    executor = MigrationExecutor(connection)
-    targets = executor.loader.graph.leaf_nodes()
-    return not executor.migration_plan(targets)
+    try:
+
+        connection = connections[DEFAULT_DB_ALIAS]
+        connection.prepare_database()
+        executor = MigrationExecutor(connection)
+        targets = executor.loader.graph.leaf_nodes()
+        return not executor.migration_plan(targets)
+
+    except:  # noqa
+
+        return False
