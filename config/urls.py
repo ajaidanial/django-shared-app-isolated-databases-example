9ca78a9120e6af4fc34076c7d271b83c251b8a8c
ajaidanial <ajaidanial@gmail.com>
2022-02-17 19:19:15 +0530

    added init apply

--- config/urls.py
+++ config/urls.py
@@ -1,7 +1,10 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.core.management import call_command
 from django.urls import include, path
+
+from main_app import helpers
 
 # Base URL's
 # ------------------------------------------------------------------------------
@@ -14,3 +17,16 @@ urlpatterns = [
 # ------------------------------------------------------------------------------
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Apply all the necessary init app configurations
+# ------------------------------------------------------------------------------
+if helpers.is_default_database_synchronized():
+    from main_app.models import UserDatabaseTracker
+
+    for tracker in UserDatabaseTracker.objects.all():
+        db_name = tracker.database_name
+        settings.DATABASES[db_name] = {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": settings.BASE_DIR / f"{db_name}.sqlite3",
+        }
+        call_command("migrate", database=db_name)
