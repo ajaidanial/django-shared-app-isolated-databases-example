476a0797fb90b07e8b61ebe3a09091bebb183468
ajaidanial <ajaidanial@gmail.com>
2022-02-17 20:33:12 +0530

    added naming

--- main_app/urls.py
+++ main_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("ping/", views.ping_view),
-    path("register/", views.UserRegistrationView.as_view()),
+    path("register/", views.UserRegistrationView.as_view(), name="register_view"),
+    path("login/", views.UserLoginView.as_view(), name="login_view"),
 ]
