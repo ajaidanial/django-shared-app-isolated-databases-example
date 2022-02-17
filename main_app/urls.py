af2e4c0751656efd52a304c3379f223875034776
ajaidanial <ajaidanial@gmail.com>
2022-02-17 20:51:37 +0530

    fixed app from breakage

--- main_app/urls.py
+++ main_app/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("ping/", views.ping_view),
+    path("ping/", views.ping_view, name="ping_view"),
     path("register/", views.UserRegistrationView.as_view(), name="register_view"),
     path("login/", views.UserLoginView.as_view(), name="login_view"),
 ]
