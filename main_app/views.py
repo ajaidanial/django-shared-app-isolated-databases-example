f4e73346b604850c18c28e55133f8254d87aa8d9
ajaidanial <ajaidanial@gmail.com>
2022-02-17 21:22:39 +0530

    fixed view

--- main_app/views.py
+++ main_app/views.py
@@ -1,16 +1,18 @@
 from django.contrib.auth import get_user_model
 from django.contrib.auth.views import LoginView
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse_lazy
 from django.views.generic import FormView
 
 from main_app.forms import UserRegistrationForm
+from main_app.helpers import app_login
 from main_app.models import UserDatabaseTracker
 
 
 def ping_view(request):
     """Just a ping view."""
 
+    # TODO: print data, create dummy data in users database
     return HttpResponse("Pong!")
 
 
@@ -19,6 +21,12 @@ class UserLoginView(LoginView):
 
     success_url = reverse_lazy("ping_view")
     template_name = "user_login_form.html"
+
+    def form_valid(self, form):
+        """Login and set the db on request."""
+
+        app_login(request=self.request, user=form.get_user())
+        return HttpResponseRedirect(self.get_success_url())
 
 
 class UserRegistrationView(FormView):
@@ -38,11 +46,12 @@ class UserRegistrationView(FormView):
         database_name = form.cleaned_data["username"]
 
         # create and handle the database
-        UserDatabaseTracker.objects.create(
+        tracker = UserDatabaseTracker.objects.create(
             user_identifier=database_name, database_name=database_name
-        ).set_up_database_and_configurations()
+        )
+        tracker.set_up_database_and_configurations()
 
         # create the user on the user's database
-        get_user_model().objects.create_user(**form.cleaned_data, use_db=database_name)
+        get_user_model().objects.create_user(**form.cleaned_data, use_db=tracker.db)
 
         return super(UserRegistrationView, self).form_valid(form=form)
