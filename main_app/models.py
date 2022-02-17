6fa3c6dcab9a76fd5145dfa453b40988cb945685
ajaidanial <ajaidanial@gmail.com>
2022-02-17 20:47:39 +0530

    fixed models

--- main_app/models.py
+++ main_app/models.py
@@ -1,9 +1,27 @@
 from django.conf import settings
+from django.contrib.auth.models import AbstractUser
 from django.core.management import call_command
-from django.db import models, DEFAULT_DB_ALIAS
+from django.db import DEFAULT_DB_ALIAS, models
+
+from main_app.managers import AppUserManagerQuerySet, BaseObjectManagerQuerySet
 
 
-class UserDatabaseTracker(models.Model):
+class BaseModel(models.Model):
+    """Base class for easy implementations."""
+
+    class Meta:
+        abstract = True
+
+    objects = BaseObjectManagerQuerySet.as_manager()
+
+
+class AppUser(AbstractUser):
+    """Custom app's User model. Overridden for easy implementations."""
+
+    objects = AppUserManagerQuerySet.as_manager()
+
+
+class UserDatabaseTracker(BaseModel):
     """Table to track which database belongs to which user."""
 
     use_db = DEFAULT_DB_ALIAS
@@ -15,6 +33,8 @@ class UserDatabaseTracker(models.Model):
         """Adds the db to the settings and creates the db if necessary."""
 
         db_name = self.database_name
+
+        print(f"Initializing database: {db_name}")
         settings.DATABASES[db_name] = {
             "ENGINE": "django.db.backends.sqlite3",
             "NAME": settings.BASE_DIR / f"{db_name}.sqlite3",
